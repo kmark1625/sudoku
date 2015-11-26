b1ab915e5f13cd32ae65caf44b6aac18cd6ff7a4
Fong <fong.fan.90@gmail.com>
2015-11-26 14:00:32 -0600

    specs started

--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -1,2 +1,54 @@
 require_relative 'sudoku'
 
+describe Sudoku do
+  describe '#possible_numbers_for_cell' do
+
+  end
+
+  describe '#is_possible_in_subsquare' do
+
+  end
+
+  describe '#is_possible_in_subsquare' do
+  end
+
+  describe '#is_possible_in_row' do
+
+  end
+
+  describe '#is_possible_in_column' do
+    it 'returns false when the number exists in the column' do
+      board = [[6,'-',8,7,3,'-','-','-','-'],
+              [2,'-','-','-','-','-',4,6,'-'],
+              ['-','-','-','-',6,4,8,2,'-'],
+              ['-',8,'-','-','-',5,7,'-',1],
+              [9,'-','-',6,1,8,'-','-',4],
+              ['-',3,1,'-','-','-','-',8,'-'],
+              [8,6,'-',2,'-','-','-',3,9],
+              ['-',5,'-','-','-','-',1,'-','-'],
+              [1,'-','-',4,5,6,2,'-','-']]
+      i = 1
+      j = 2
+      num = 8
+
+      expect(Sudoku.is_possible_in_column(board,i,j,num)).to be_falsey
+    end
+
+    it 'returns true when the number does not exist in the column' do
+      board = [[6,'-',8,7,3,'-','-','-','-'],
+              [2,'-','-','-','-','-',4,6,'-'],
+              ['-','-','-','-',6,4,8,2,'-'],
+              ['-',8,'-','-','-',5,7,'-',1],
+              [9,'-','-',6,1,8,'-','-',4],
+              ['-',3,1,'-','-','-','-',8,'-'],
+              [8,6,'-',2,'-','-','-',3,9],
+              ['-',5,'-','-','-','-',1,'-','-'],
+              [1,'-','-',4,5,6,2,'-','-']]
+      i = 1
+      j = 2
+      num = 9
+
+      expect(Sudoku.is_possible_in_column(board,i,j,num)).to be_truthy
+    end
+  end
+end
