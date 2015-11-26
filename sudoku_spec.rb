3ced2173b638ac4910b8068e3ff307146a79ac6b
Kevin Mark <kmark1625@gmail.com>
2015-11-26 14:47:53 -0600

    Add subsquare methods

--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -6,10 +6,42 @@ describe Sudoku do
   end
 
   describe '#is_possible_in_subsquare' do
+    it 'returns false when the number exists in the subsquare' do
+      puzzle = Sudoku.new('')
+      puzzle.board = [[6,'-',8,7,3,'-','-','-','-'],
+              [2,'-','-','-','-','-',4,6,'-'],
+              ['-','-','-','-',6,4,8,2,'-'],
+              ['-',8,'-','-','-',5,7,'-',1],
+              [9,'-','-',6,1,8,'-','-',4],
+              ['-',3,1,'-','-','-','-',8,'-'],
+              [8,6,'-',2,'-','-','-',3,9],
+              ['-',5,'-','-','-','-',1,'-','-'],
+              [1,'-','-',4,5,6,2,'-','-']]
+      i = 1
+      j = 2
+      num = 8
 
-  end
+      expect(puzzle.is_possible_in_subsquare(i,j,num)).to be_falsey
+    end
 
   describe '#is_possible_in_subsquare' do
+    it 'returns true when the number does not exist in the subsquare' do
+      puzzle = Sudoku.new('')
+      puzzle.board = [[6,'-',8,7,3,'-','-','-','-'],
+              [2,'-','-','-','-','-',4,6,'-'],
+              ['-','-','-','-',6,4,8,2,'-'],
+              ['-',8,'-','-','-',5,7,'-',1],
+              [9,'-','-',6,1,8,'-','-',4],
+              ['-',3,1,'-','-','-','-',8,'-'],
+              [8,6,'-',2,'-','-','-',3,9],
+              ['-',5,'-','-','-','-',1,'-','-'],
+              [1,'-','-',4,5,6,2,'-','-']]
+      i = 1
+      j = 2
+      num = 9
+
+      expect(puzzle.is_possible_in_subsquare(i,j,num)).to be_truthy
+    end
   end
 
   describe '#is_possible_in_row' do
