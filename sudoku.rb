2186316ec3e51910d714e680208ecb094e409b64
Kevin Mark <kmark1625@gmail.com>
2015-11-26 12:36:53 -0600

    Add files

---
+++ sudoku.rb
@@ -0,0 +1,29 @@
+#Takes a board as a string in the format
+#you see in the puzzle file. Returns
+#something representing a board after
+#your solver has tried to solve it.
+def solve(board_string)
+end
+
+#Returns a boolean indicating whether
+#or not the provided board is solved.
+#The provided board will be in whatever
+#form `solve` returns.
+def solved?(board)
+end
+
+#Takes in a board in some form and
+#returns a string that's well formatted
+#for output to the screen. No `puts` here!
+#The provided board will be in whatever
+#form `solve` returns.
+def pretty_board(board)
+end
+
+#Takes in a board in some form and
+#returns a string in the same format
+#as you see in the puzzle file.
+#The provided board will be in whatever
+#form `solve` returns.
+def board_to_string(board)
+end
