e4414416c1a1f971e0f062a0543a144cc3021171
Kevin Mark <kmark1625@gmail.com>
2015-11-26 14:05:44 -0600

    Clean up comments

---
+++ psuedocode.rb
@@ -0,0 +1,11 @@
+# PSEUDO-CODE
+=begin
+LOOP until a number has not been added on a full pass of the board or solved
+  - Go through every blank single cell on the board
+  - Generate a list of possible numbers
+    - Possible numbers can be eliminated based on row, col, grid and ???
+=end
+=begin
+Other ideas to solve advanced problems:
+Make a guess and loop through board and check if a number can be added.  Keep making guesses using recursion.
+=end
--- sudoku.rb
+++ sudoku.rb
@@ -1,31 +1,18 @@
 class Sudoku
-  #Takes a board as a string in the format
-  #you see in the puzzle file. Returns
-  #something representing a board after
-  #your solver has tried to solve it.
+  attr_reader :board
+
+  def initialize(board_string)
+  end
+
   def solve(board_string)
   end
 
-  #Returns a boolean indicating whether
-  #or not the provided board is solved.
-  #The provided board will be in whatever
-  #form `solve` returns.
   def solved?(board)
   end
 
-  #Takes in a board in some form and
-  #returns a string that's well formatted
-  #for output to the screen. No `puts` here!
-  #The provided board will be in whatever
-  #form `solve` returns.
   def pretty_board(board)
   end
 
-  #Takes in a board in some form and
-  #returns a string in the same format
-  #as you see in the puzzle file.
-  #The provided board will be in whatever
-  #form `solve` returns.
   def board_to_string(board)
   end
 
